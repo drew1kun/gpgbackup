50812ffcf647f5ddc18387a55c0641bb5d3afa8a
Drewshg312 <drewshg@gmail.com>
2018-05-17 00:35:00 -0700

    first one

---
+++ README.md
@@ -0,0 +1,71 @@
+# gpgbackup
+Back up your gpg private keys to printable QR codes, and restore them from scanned PNG images.
+
+
+## What does it do?
+
+The script will create a directory with your gpg `<Key_ID>` name and put all the files there.
+`<Key_ID>` - gpg key identity: may be an email or key fingerprint like: `5F8C67D3`
+
+For recovering of the key the `gpgbackup.sh` script searches for a directory named `<Key_ID>`, containig PNG files with
+QR-Codes. It's just that simple.
+
+**IMPORTANT:**
+
+1 - Order of PNG files matters! PNGs must be numbered properly, so the first part of key goes first and so on.
+
+2 - When recovering secret keys from scanned PNG files with QR-codes, the public portion of a key must be in gpg
+keychain.
+
+
+## Dependencies
+Depends on:
+
+  - **gpg/gpg2**
+  - **paperkey**
+  - **qrencode**
+  - **coreutils** *(GNU version)*
+  - **pdflatex** (mactex on MacOS or latex on Linux)
+
+MacOS: install dependencies with Homebrew:
+
+      brew cask install gpg-suite mactex
+Install GPGTools GUI with gpg/gpg CLI as well as mactex(pdflatex)
+
+      brew install coreutils qrencode paperkey ghostscript
+coreutils will be installed with g (for GNU) - prefix:  gsplit, ghead etc
+
+## Usage
+
+### BACK UP SECRET GPG KEYS TO QR CODES
+
+Option 1: Generate a tarball with 4 PNG files containing QR-Codes of specified. You can then store it on digital
+storages
+
+    ./gpgbackup.sh -k user@email.com
+
+
+Option 2: Generate a printable PDF file to store your keys on paper in safe onffine storage.
+
+    ./gpgbackup.sh -p 5F8C67D3
+
+
+### RECOVER SECRET KEYS
+
+If you stored keys on paper, then do the following:
+
+ - scan the QR-codes,
+ - save each QR-code in separate PNG file and name them accordingly:
+    **1.png, 2.png, 2.png, 4.png** (Remember: **Order matters!**)
+ - put all PNGs to folder named `<Key_ID>`
+ - Clone this repo and put the folder to the repo next to script.
+ - run: `./gpgbackup.sh -q <Key_ID>`
+
+The gpg secret key file named `<Key_ID>.asc` will be generated in the `<Key_ID>` directory.
+
+Now you can import it using:
+
+    gpg2 --import ./<Key_ID>/<Key_ID>.asc
+
+Done!
+
---
+++ gpgbackup.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+# Importing the QR codes:
+# Note that, when making scans or photographs, you do not produce large images.
+# If zbar does not recognise your QR code, try downscaling the image.
+
+#========== Define functions ===========
+function usage {
+    echo "Generate the QR Code from Secret Key and vice versa."
+    echo ""
+    echo "The script will create a directory with <Key_ID> name,"
+    echo "containing a tarball with 4 PNG QR-Code files for specified secret key."
+    echo ""
+    echo "Depends on:"
+    echo "  - gpg/gpg2"
+    echo "  - paperkey"
+    echo "  - qrencode"
+    echo "  - coreutils (GNU version)"
+    echo "  - pdflatex (mactex on MacOS or latex on Linux)"
+    echo ""
+    echo "MacOS: install dependencies with Homebrew:"
+    echo ""
+    echo "      'brew cask install gpg-suite mactex'"
+    echo "Install GPGTools GUI with gpg/gpg CLI as well as mactex(pdflatex)"
+    echo ""
+    echo "      'brew install coreutils qrencode paperkey ghostscript'"
+    echo "coreutils will be installed with g (for GNU) - prefix:  gsplit, ghead etc"
+    echo ""
+    echo "Usage: "
+    echo "      'gpg2qr.sh { -h | {-k | -q <Key_ID>} }'"
+    echo ""
+    echo "      -h, ?, --help - This message"
+    echo "      -k, --key2qr - Convert existing secret key to QR-Codes(PNG). Assumes secret key exists in gpg ring"
+    echo "      -q, --qr2key - Convert QR-Coedes to secret key. NOTE: Public key must exist on gpg pubring"
+    echo "      <Key_ID> - an email identity or the gpg key ID"
+    echo ""
+}
+
+function check_deps {
+    command -v gpg >/dev/null 2>&1 || { echo >&2 "gpg/gpg2 is not installed.  Aborting."; exit 1; }
+    command -v paperkey >/dev/null 2>&1 || { echo >&2 "paperkey is not installed.  Aborting."; exit 1; }
+    command -v qrencode >/dev/null 2>&1 || { echo >&2 "qrencode is not installed.  Aborting."; exit 1; }
+    command -v zbarimg >/dev/null 2>&1 || { echo >&2 "zbarimg (zbar) is not installed.  Aborting."; exit 1; }
+    command -v pdflatex >/dev/null 2>&1 || { echo >&2 "pdflatex is not installed.  Aborting."; exit 1; }
+}
+
+function key2qr {
+    mkdir -p ./$1
+    gpg2 --export-secret-key $1 | paperkey --output-type raw | base64 > $1.ascii
+    $2 -n 4 -d $1.ascii qr_
+    for f in qr_*; do cat ${f} | qrencode -o $1_${f}.png; done
+    tar -cvzf $1.qr.tar.gz ./*qr_*.png
+    mv $1.qr.tar.gz ./$1
+
+    # Cleanup
+    rm -rfP $1.ascii *qr_*
+    exit 0
+}
+
+function genpdf {
+    mkdir -p ./$1
+
+    # prepare gpgbackup.tex self generated latex document:
+    sed -e "s/PRIVATE_KEY/$1/g" \
+    -e "s/HEAD/$2/g" \
+    -e "s/DECODE/$3/g" \
+    -e "s/SPLIT/$4/g" ./latex/gpgbackup.tex.template > gpgbackup.tex
+
+    # generate pdf and move it right place:
+    pdflatex --shell-escape gpgbackup.tex > /dev/null 2>&1
+    mv gpgbackup.pdf ./$1
+
+    # Cleanup:
+    rm -rfP gpgbackup.*
+    exit 0
+}
+
+function qr2key {
+    cd ./$1
+    for f in *.png; do zbarimg --raw $f | $2 -c -1 > $f.out.ascii ; done
+    cat *.out.ascii | base64 $3 | paperkey --pubring ~/.gnupg/pubring.gpg > "$1.asc"
+    #cat *.out.ascii | base64 $3 | paperkey --pubring ~/.gnupg/pubring.gpg | gpg2 --import
+
+    # Cleanup
+    rm -rfP *.out.ascii
+    exit 0
+}
+
+#=======================================
+
+#================ Main =================
+PRIVATE_KEY=$2
+
+# Detect OS
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+
+case ${OS} in
+    'darwin')
+        HEAD='ghead'
+        DECODE='-D'
+        SPLIT='gsplit'
+        ;;
+    'linux')
+        HEAD='head'
+        DECODE='-d'
+        SPLIT='split'
+        ;;
+    *)
+        echo "Unknown OS, Only MacOS and Linux supported for now"
+esac
+
+check_deps
+
+# All of these commands are mutually exclusive, so we test for a single param here:
+if [[ $# -lt 1 ]]; then
+    echo "Please choose AT LEAST ONE option -k | -p | -q. See gpg2qr.sh -h"
+    exit 1
+elif [[ $# -gt 2 ]]; then
+    echo "Please choose ONLY ONE option: -k | -p | -q option. See gpg2qr.sh -h"
+    exit 1
+fi
+
+# Parse Arguments
+while getopts ':hk:p:q:' opt; do
+    case ${opt} in
+        k)
+            key2qr "${PRIVATE_KEY}" "${SPLIT}"
+            ;;
+        q)
+            qr2key "${PRIVATE_KEY}" "${HEAD}" "${DECODE}"
+            ;;
+        p)
+            genpdf "${PRIVATE_KEY}" "${HEAD}" "${DECODE}" "${SPLIT}"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option -${OPTARG}. Try 'gpg2qr.sh -h' first"
+            exit 0
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+#=======================================
